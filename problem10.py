7aa4fb29d45da7a7e825cf6feafbb78d44d0595c
Francisco Edenilson <edenilson.victoralves@outlook.com>
2018-07-22 22:34:17 -0300

    Update problem working 50%
    This resolution are working, but just with small values.
    
    Yet in analisis.

--- problem10.py
+++ problem10.py
@@ -1,11 +1,12 @@
 num = 1
 tot = 0
 temp = 0
-for c in range(1, 10):
-    if num % c == 0:
-        tot += 1
-if tot <= 2 and tot !=1:
-    print(num)
-
-
-
+while num < 2000000:
+    tot = 0
+    for c in range(1, num+1):
+        if num % c == 0:
+            tot += 1
+    if tot <= 2 and num != 1:
+        temp += num
+    num += 1
+print('A soma dos números primos abaixo de 10 é: {}'.format(temp))
