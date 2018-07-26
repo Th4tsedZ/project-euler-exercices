50e512895046533ffe400bf34285f0ae1c294757
Francisco Edenilson <edenilson.victoralves@outlook.com>
2018-07-25 22:25:34 -0300

    Addition of statement
    Addition of statement in every exercise for a better understanding.

--- problem1.py
+++ problem1.py
@@ -1,3 +1,8 @@
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
 soma = 0
 for c in range(1, 1000):
     if c%3 == 0 or c%5 == 0:
--- problem16.py
+++ problem16.py
@@ -1,3 +1,8 @@
+'''
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?
+'''
 n = 2**1000
 lista = [int(i) for i in str(n)]
 soma = sum(lista)
